24af774c7438bb3afece49bfde3c3bd893a6883d
Yuta-NoobCoder <ukiyutaM4A1@gmail.com>
2020-10-20 23:47:31 +0900

    driveメソッドの変更に伴う修正

--- No_3/Q3/Driver.java
+++ No_3/Q3/Driver.java
@@ -2,16 +2,13 @@ public class Driver
 {
     public static void main(String[] args)
     {
-        Car car1 = new Car();
-        Car car2 = new Car();
-
-        double distance;
-
         //ナンバー5555の車を初期化
+        Car car1 = new Car();
         car1.num = 5555;
         car1.gas = 0;
         car1.kyori = 0;
         //ナンバー9999の車を初期化
+        Car car2 = new Car();
         car2.num = 9999;
         car2.gas = 0;
         car2.kyori = 0;
@@ -19,36 +16,29 @@ public class Driver
         car1.kyuyu(10.0);
         car2.kyuyu(8.0);
         //運転(car1)
-        distance = 100.0;
-        car1.drive(distance);
-        if(car1.gas < 0) 
+        if(car1.drive(100) == -1) 
         {
             System.out.println("ガス欠で途中で止まりました");
-            car1.gas = 0;
         } 
-        else System.out.println(distance + "km走行できました");
+        else System.out.println("100km走行できました");
         car1.show();
 
         //運転(car2)
-        distance = 150.0;
-        car2.drive(distance);
-        if(car2.gas < 0)
+        if(car2.drive(150) == -1)
         {
             System.out.println("ガス欠で途中で止まりました");
-            car2.gas = 0.0;
         }
-        else System.out.println(distance + "km走行できました");
+        else System.out.println("150km走行できました");
         car2.show();
+
         //給油・運転(car2)
         car2.kyuyu(20.0);
-        distance = 200.0;
-        car2.drive(distance);
-        if(car2.gas < 0) 
+        
+        if(car2.drive(200) == -1) 
         {
             System.out.println("ガス欠で途中で止まりました");
-            car2.gas = 0;
         }
-        else System.out.println(distance + "km走行できました");        
+        else System.out.println("200km走行できました");        
         car2.show();
     }
 }
