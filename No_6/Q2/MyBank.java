77d8ce27dba79d5e04f78a30c664bd7b8b0a2047
Yuta-NoobCoder <ukiyutaM4A1@gmail.com>
2020-11-04 11:23:23 +0900

    軽微な修正

--- No_6/Q2/MyBank.java
+++ No_6/Q2/MyBank.java
@@ -2,27 +2,24 @@ import java.util.Hashtable;
 
 public class MyBank {
   public static void main(String[] args) {
+
     Hashtable<String, Account> bank = new Hashtable<String, Account>();
+
+    //口座を生成
     bank.put("一郎", new Account("一郎"));
     bank.put("三郎", new Account("三郎"));
     bank.put("太郎", new Account("太郎"));
     bank.put("花子", new Account("花子"));
-    Account account = bank.get("三郎");
-    account.deposit(1000);
-    account = bank.get("花子");
-    account.deposit(5000);
-    account = bank.get("三郎");
-    account.deposit(3000);
-    account = bank.get("花子");
-    account.deposit(2000);
 
-    account = bank.get("一郎");
-    System.out.println("一郎の口座の残高：" + account.showBalance());
-    account = bank.get("三郎");
-    System.out.println("三郎の口座の残高：" + account.showBalance());
-    account = bank.get("太郎");
-    System.out.println("太郎の口座の残高：" + account.showBalance());
-    account = bank.get("花子");
-    System.out.println("花子の口座の残高：" + account.showBalance());
+    //預金処理
+    (bank.get("三郎")).deposit(1000);
+    (bank.get("花子")).deposit(5000);
+    (bank.get("三郎")).deposit(3000);
+    (bank.get("花子")).deposit(2000);
+
+    System.out.println("一郎の口座の残高：" + (bank.get("一郎")).showBalance());
+    System.out.println("三郎の口座の残高：" + (bank.get("三郎")).showBalance());
+    System.out.println("太郎の口座の残高：" + (bank.get("太郎")).showBalance());
+    System.out.println("花子の口座の残高：" + (bank.get("花子")).showBalance());
   }
 }
